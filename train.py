89b79c7049442ae189115d226b6c317399360edc
keshav1017 <prasadkeshav003@gmail.com>
2025-07-08 19:16:20 +0530

    Sampling done with the huggingface parameters of gpt2

--- train.py
+++ train.py
@@ -41,7 +41,7 @@ class CasualSelfAttention(nn.Module):
         q, k, v = qkv.split(self.n_embd, dim=2)
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        v = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         y = F.scaled_dot_product_attention(q, k, v, is_causal=True) # flask attention
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assembke all head outputs side by side
 
@@ -94,6 +94,30 @@ class GPT(nn.Module):
         ))
 
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+    def forward(self, idx, targets=None):
+        # idx is of shape (B, T)
+        B, T = idx.shape
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
+
+        # forward the token and position embeddings
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device) # shape (T)
+        pos_emb = self.transformer.wpe(pos) # position embeddings  of shape (T, n_embd)
+        tok_emb = self.transformer.wte(idx) # token embeddings of shape (B, T, n_embd)
+        x = tok_emb + pos_emb
+
+        # forward the blocks of the transformer
+        for block in self.transformer.h:
+            x = block(x)
+
+        # forward the final layernorm and the classifier
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x) # (B, T, vocab_size)
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        
+        return logits, loss
 
     @classmethod
     def from_pretrained(cls, model_type):
@@ -151,5 +175,59 @@ class GPT(nn.Module):
         return model
 
 # -------------------------------------------------------------------------------------
+
+# attempt to auto detect the decvice
+device = "cpu"
+if torch.cuda.is_available():
+    device = "cuda"
+
+num_return_sequences = 5
+max_length = 30
+
 model = GPT.from_pretrained('gpt2')
-print("worked well, yay!")
+# model = GPT(GPTConfig())
+model.eval()
+model.to(device)
+
+# prefix tokens
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+tokens = enc.encode("Hello, I'm a language model,")
+tokens = torch.tensor(tokens, dtype=torch.long) # (8,)
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # (5, 8)
+x = tokens.to(device)
+
+# generate! right now x is (B, T) where B = 5, T = 8
+# set the seed to 42
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+while x.size(1) < max_length:
+    # forward the model to get the logits
+    with torch.no_grad():   
+        logits, _ = model(x) # (B, T, vocab_size)
+
+        # take the logits at the last position
+        logits = logits[:, -1, :] # (B, vocab_size)
+
+        # get the probabilities
+        probs = F.softmax(logits, dim=-1)
+
+        # do top-k sampling of 50 (huggingface pipeline default)
+        # topk_probs here becomes (5, 50), topk_indices is (5, 50)
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+
+        # select a token from top-k probabilities
+        ix = torch.multinomial(topk_probs, 1) # (B, 1)
+
+        # gather the corresponding indices
+        xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
+
+        # append to the sequence
+        x = torch.cat((x, xcol), dim=1)
+
+# print the generated text
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(f"> {decoded}")
